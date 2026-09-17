315f5b1712f6dbbcac2c6f3a3086168eca23388e
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-09-17 19:40:49 +0700

    feat: add Footer component and update layout styles

--- src/feature/container/PageContainer.tsx
+++ src/feature/container/PageContainer.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/feature/components/Hero";
+import Footer from "@/feature/components/Footer";
 
 const PageContainer = () => {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   );
 };

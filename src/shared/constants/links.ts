a88d2bba89c69c61ff6f288b03ffe1b3e32e7661
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-09-18 15:45:55 +0700

    feat: update GUIDEBOOK_URL for improved accuracy and consistency

--- src/shared/constants/links.ts
+++ src/shared/constants/links.ts
@@ -1,2 +1,2 @@
 export const REGISTRATION_URL = "https://forms.gle/7MeYKZoor7C5ZMEV8";
-export const GUIDEBOOK_URL = "/files/Guidebook DSMBC + Tata Tertib 2026.pdf";
+export const GUIDEBOOK_URL = "/files/Guidebook-DSMBC-Tata-Tertib-2026.pdf";
